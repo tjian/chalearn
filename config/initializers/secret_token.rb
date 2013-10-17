894d3a10d9727cc2d8b7bc4a6b1aab3faee3ddb3
Thanabordi Jianrungsang <thanabordi@berkeley.edu>
2013-10-16 19:32:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chalearn::Application.config.secret_token = 'ed56b4d8abbb78aa411fae6c5d0f55eef76f1a9b080698c1a307c87f6b34480a5f65ec7e48a7f590f37faba8435e32751a162637e3e077c76c5967968ef3d244'
